aa3ceaf7705164a7b5e0521ba09c8a56e3341738
chinaza-Sommie <chinazachukwunweike35@gmail.com>
2024-07-17 14:40:41 +0100

    initial pages

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    // #111A24 and yellow #F8EE00
+    <main className="">
+      hello world
+    </main>
+  );
+}
